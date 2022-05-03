39713fcbb0d13a7435cd2c20b6a9d3ee2780640c
Ismayil Ismayilov <iismayilov4574@gmail.com>
2022-05-03 21:09:15 +0300

    Task 1.3 works locally
    On VM this either times out or returns 1 hop. No idea why

--- Project 3/task_1-3.py
+++ Project 3/task_1-3.py
@@ -1,24 +1,29 @@
-from scapy.all import *
+import scapy.all as scapy
 
 import sys
 
-INTERFACE_NAME = 'br-22184f2413c5'
+MAX_TTL = 255
 
-dst_ip = sys.argv[1]
+dst = sys.argv[1]
 
 cur_ttl = 1
 
-while True:
-    a = IP(dst=dst_ip, ttl=cur_ttl)
-    b = ICMP()
+while cur_ttl < MAX_TTL:
+    a = scapy.IP(dst=dst, ttl=cur_ttl)
+    b = scapy.ICMP()
 
     pkt = a / b
 
-    reply = sr1(pkt, verbose=0, iface=INTERFACE_NAME)
+    reply = scapy.sr1(pkt, verbose=0, timeout=2)
 
-    if reply[ICMP].type == 11:
-        print('{} => {}'.format(cur_ttl, reply[IP].src))
-        ttl += 1
-    elif reply[ICMP].type == 0:
-        print('{} => {}'.format(cur_ttl, reply[IP].src))
+    if not reply:
+        print('{} => * * * *'.format(cur_ttl))
+        cur_ttl += 1
+        continue
+
+    if reply[scapy.ICMP].type == 11:
+        print('{} => {}'.format(cur_ttl, reply[scapy.IP].src))
+        cur_ttl += 1
+    elif reply[scapy.ICMP].type == 0:
+        print('{} => {}'.format(cur_ttl, reply[scapy.IP].src))
         break
